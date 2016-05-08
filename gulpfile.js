afcaeb79f5595d8bbae80b6ba27dd14c7acae4aa
Andrew <ajwippl@gmail.com>
2016-05-08 16:01:43 -0400

    Add jshint, jscs, and gulp-util

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var util = require('gulp-util');
 
-gulp.task('hello-world', function() {
-    console.log('Our first hello world gulp task!');
-})
+gulp.task('vet', function() {
+    gulp.src([
+        './src/**/*.js',
+        './*.js'
+        ])
+        .pipe(jscs())
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
+});
+
+/////////////////
+
+function log(msg) {
+
+}
